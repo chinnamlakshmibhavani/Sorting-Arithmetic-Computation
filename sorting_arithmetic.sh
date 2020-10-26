76cc9cce24d1a3b267c3d520eaaaa3b40fc21cfa
chinnamlakshmibhavani <chinnamlakshmi1161999@gmail.com>
2020-10-26 23:03:19 +0530

    adding code to compute c+a/b

--- sorting_arithmetic.sh
+++ sorting_arithmetic.sh
@@ -3,5 +3,8 @@ echo "Welcome to arthmetic operations"
 read -p "Enter number 1:" a
 read -p "Enter number 2:" b
 read -p "Enter number 3:" c
-computation2=$(( $a*$b+$c ))
-echo "second Computation is:"$computation2
+
+computation3=$(( $c+$a/$b ))
+echo "third computation is:"$computation3
+
+
