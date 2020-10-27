cc43ffbaf1e0f70ea5f35de45e89c6170cc8f690
chinnamlakshmibhavani <chinnamlakshmi1161999@gmail.com>
2020-10-27 08:24:25 +0530

    adding code to store the computational values in dictionary

--- sorting_arithmetic.sh
+++ sorting_arithmetic.sh
@@ -3,10 +3,21 @@ echo "Welcome to arthmetic operations"
 read -p "Enter number 1:" a
 read -p "Enter number 2:" b
 read -p "Enter number 3:" c
-
+computation1=$(( $a+$b*$c ))
+echo "first computation is:" $computation1
+computation2=$(( $a*$b+$c ))
+echo "second Computation is:"$computation2
+computation3=$(( $c+$a/$b ))
+echo "third computation is:"$computation3
 computation4=$(( $a%$b+$c ))
 echo "Fourth Computation is:"$computation4
-
+variable=${computation[@]}
+for values in $variable
+do
+   array[count]=$values
+   count=$(( $count+1 ))
+done
+echo "array values are :"${array[@]}
 
 
 
