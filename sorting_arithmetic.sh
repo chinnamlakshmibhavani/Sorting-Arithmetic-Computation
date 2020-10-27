1d42bf3ac85dee4eb45ab6a253846cee168ec70a
chinnamlakshmibhavani <chinnamlakshmi1161999@gmail.com>
2020-10-27 08:33:58 +0530

    adding code to sort the values of computations in descending order

--- sorting_arithmetic.sh
+++ sorting_arithmetic.sh
@@ -27,4 +27,11 @@ do
 done
 echo "array values are :"${array[@]}
 
+sort=`echo ${array[@]} | awk 'BEGIN{RS=" ";} {print $1}' | sort -n -r`
+counter=0
+for values in $sort
+do
+sortedarray[((counter++))]="$values"
+done
+echo "sorted array in descending order are:" ${sortedarray[@]}
 
